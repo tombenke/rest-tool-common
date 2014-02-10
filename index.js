513fb51d06c8cf7eb00bcdf4fcf988963ee7e9cf
Tamás Benke <tombenke@gmail.com>
2014-02-10 08:14:35 +0100

    Bugfix: wrong reference to lib modules from index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.extend = require('lib/extend');
-exports.generator = require('lib/generator');
-exports.services = require('lib/services');
+exports.extend = require('./lib/extend');
+exports.generator = require('./lib/generator');
+exports.services = require('./lib/services');
